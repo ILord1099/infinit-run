15751082b0acf2b779471c0b7843782a4204347f
ILord1099 <78938222+ILord1099@users.noreply.github.com>
2023-02-25 02:22:37 -0300

    Move and Light
    Movimentação basica, spawn de plataformas e luz.
    tudo com configuração basica.

---
+++ Runner3D/Assets/Script/Player.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{   
+    private CharacterController controller;
+
+    private float jumpVelocity;
+    public float speed;
+    public float jumpHeight;
+    public float gravity;
+    public float horizontalSpeed;
+    private bool isMovingLeft;
+    private bool isMovingRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();// pega o componente character controler que esta anexado ao script    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = Vector3.forward * speed;// adiciona "1" no Eixo Z de profundidade 
+
+        if(controller.isGrounded)
+        {
+                if(Input.GetKeyDown(KeyCode.Space))
+                {
+                    jumpVelocity=jumpHeight;
+                }
+
+                if(Input.GetKeyDown(KeyCode.RightArrow)&& transform.position.x <3f && !isMovingRight)
+                {
+                    isMovingRight = true;
+                    StartCoroutine(RightMove()); 
+                }
+                if(Input.GetKeyDown(KeyCode.LeftArrow)&& transform.position.x > -3f && !isMovingLeft)
+                {
+                    isMovingLeft = true;
+                    StartCoroutine(LeftMove());
+                }
+        }
+        else
+        {
+            jumpVelocity -=gravity;
+        }
+
+        direction.y = jumpVelocity;
+
+        controller.Move(direction*Time.deltaTime);
+    }
+
+    IEnumerator LeftMove()// courrotine pode ser pausada e controlada por tempo
+    {
+        for (float i = 0; i < 10; i+= 0.1f)
+        {
+            controller.Move(Vector3.left *Time.deltaTime * horizontalSpeed);
+            yield return null;
+        }
+        isMovingLeft = false;
+    }
+    IEnumerator RightMove()
+    {
+        for(float i = 0; i < 10; i+= 0.1f)
+        {
+            controller.Move(Vector3.right * Time.deltaTime * horizontalSpeed);
+            yield return null;
+
+        }
+        isMovingRight = false;
+    }
+}
---
+++ Runner3D/Assets/Script/SpawnPlatform.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlatform : MonoBehaviour
+{
+        public List<GameObject> platforms = new List<GameObject>();//responsavel pelas plataformas prefabs
+        public List<Transform> currentPlatforms = new List<Transform>();// objetos instanciados
+
+        public int offset;
+        private int platformIndex;
+        private Transform player;
+        private Transform currentPlatformPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        player = GameObject.FindGameObjectWithTag("Player").transform; 
+        for(int i = 0; i < platforms.Count; i++)
+        {
+           Transform p = Instantiate(platforms[i],new Vector3(0,0, i * 86),transform.rotation).transform;
+           currentPlatforms.Add(p);
+            offset += 86;
+        }
+
+        currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<Platforms>().point;
+    }
+
+    // Update is called once per frame
+    void Update() 
+    {
+        float distance = player.position.z - currentPlatformPoint.position.z;
+
+        if(distance >= 5)
+        {
+            Recycle(currentPlatforms[platformIndex].gameObject);
+            platformIndex++;
+            {
+                if(platformIndex > currentPlatforms.Count - 1)
+                {
+                        platformIndex = 0;
+                }
+            }
+
+            currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<Platforms>().point;
+        }
+    }
+    public void Recycle(GameObject platform)
+    {
+        platform.transform.position = new Vector3(0,0,offset);
+        offset += 86; 
+    }
+}
